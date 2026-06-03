ea71ff68d3384048935daa000217377522043493
Emily Richardson <ekrichardson18@gmail.com>
2026-06-03 08:46:12 -0400

    wrap up module 2 - views and urls

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
-    path("<month>", views.monthly_challenge)
+    path("", views.monthly_challenges_home_page),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
+monthly_challenges = {
+    'january': 'Eat no meat for the entire month!',
+    'february': 'Walk for at least 20 minutes a day!',
+    'march': 'Learn Django for at least 20 minutes a day!',
+    'april': 'Eat no meat for the entire month!',
+    'may': 'Walk for at least 20 minutes a day!',
+    'june': 'Learn Django for at least 20 minutes a day!',
+    'july': 'Eat no meat for the entire month!',
+    'august': 'Walk for at least 20 minutes a day!',
+    'september': 'Learn Django for at least 20 minutes a day!',
+    'october': 'Eat no meat for the entire month!',
+    'november': 'Walk for at least 20 minutes a day!',
+    'december': 'Learn Django for at least 20 minutes a day!',
+}
 # Create your views here.
 
+def monthly_challenges_home_page(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        # iterate over the keys to create the list items
+        list_items += f'<li><a href="{reverse("month-challenge", args=[month])}">{month.capitalize()}</a></li>'
+
+    response_data = f"""
+        <ul>
+            {list_items}
+        </ul>
+    """
+    return HttpResponse(response_data)
 
 # function accepts a request
 def january(request):
@@ -13,16 +42,27 @@ def january(request):
 def february(request):
     return HttpResponse("This is the request for February!")
 
+def monthly_challenge_by_number(request, month):
+    # will return a list of keys, first key in the dictionary will be first item in list
+    months = list(monthly_challenges.keys())
+    # check to see if month is a valid index
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month")
+    
+    # forward the user to the specific month that was selected
+    redirect_month = months[month - 1]
+    redirect_url = reverse("month-challenge", args=[redirect_month])
+    # want to redirect, not send a response
+    return HttpResponseRedirect(redirect_url)
+
 # dynamically decides which response to send without hardcoding 
 def monthly_challenge(request, month):
-    challenge_text = None
+    try:
+        # access using key in monthly challenges dictionary
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound('<h1>This month is not supported.</h1>')
+    
 
-    if month == 'january':
-        challenge_text = 'Eat no meat for the entire month!'
-    elif month == 'february':
-        challenge_text = 'Walk for 20 minutes every day!'
-    elif month == 'march': 
-        challenge_text = 'Learn Django for at least 20 minutes a day!'
-    else:
-        return HttpResponseNotFound('This month is not supported')
-    return HttpResponse(challenge_text)
